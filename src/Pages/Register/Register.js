480c1de837b854043c102d2c354ed3d201738094
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-08 13:46:43 +0600

    Reister authh complleete

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import app from "../../firebase/firebase.config";
+import { AuthContext } from "../../Contexts/AuthPovider/AuthProvider";
+
+
+
+const auth = getAuth(app);
+
 
 const Register = (props) => {
+
+    const { createUser, providerLogin, updateUserProfile, setUser } =
+    useContext(AuthContext);
+  
+  const { error, setError } = useState();
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const name = form.username.value;
+    const password = form.password.value;
+    const photo_url = form.photo_url.value;
+      console.log(email, name, password, photo_url);
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        handleUpdateUserProfile(name, photo_url);
+        setError("");
+        form.reset();
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.log("error:", error);
+      });
+    // handleUserProfile
+    const handleUpdateUserProfile = (name, photo_url) => {
+      const profile = {
+        displayName: name,
+        photoURL: photo_url,
+      };
+      updateUserProfile(profile)
+        .then(() => {})
+        .catch((error) => console.error(error));
+    };
+  };
+
+
+
     return (
       <div className="">
         <div className="flex my-10  justify-center">
           <div className=" w-[380px] p-8 space-y-3 rounded-xl dark:bg-gray-500 text-white">
             <h1 className="text-4xl font-bold text-center">Register</h1>
             <form
+              onSubmit={handleSubmit}
               novalidate=""
               action=""
               className="space-y-6 ng-untouched ng-pristine ng-valid"
@@ -19,8 +71,21 @@ const Register = (props) => {
                 <input
                   type="text"
                   name="username"
-                                id="username"
-                                required
+                  id="username"
+                  required
+                  placeholder="Username"
+                  className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
+                />
+              </div>
+              <div className="space-y-1 text-sm">
+                <label for="Email" className="block text-white">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
                   placeholder="Username"
                   className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
                 />
@@ -31,9 +96,9 @@ const Register = (props) => {
                 </label>
                 <input
                   type="text"
-                  name="photoUrl"
-                                id="photoUrl"
-                                required
+                  name="photo_url"
+                  id="photo_url"
+                  required
                   placeholder="Your Photo Url"
                   className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
                 />
@@ -45,8 +110,8 @@ const Register = (props) => {
                 <input
                   type="password"
                   name="password"
-                                id="password"
-                                required
+                  id="password"
+                  required
                   placeholder="Password"
                   className="w-full px-4 py-3 rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400"
                 />
