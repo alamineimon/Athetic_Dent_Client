42980cb5998491aa5765d04d1886d1f566453321
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-09 22:54:15 +0600

    review delete  done

--- src/Pages/Review/MyReview/MyRevieRow/MyReviewRow.js
+++ src/Pages/Review/MyReview/MyRevieRow/MyReviewRow.js
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const MyReviewRow = ({ rev }) => {
-    const { service, _id, serviceName, customerName, message, photoURL } = rev;
-    console.log(rev);
-    return (
-      <div>
-        <div className="container flex mt-6 flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 dark:bg-blue-900 dark:text-gray-100">
-          <div className="flex justify-between p-4">
-            <div className="flex space-x-4 mx-auto">
-              <div>
-                <img
-                  src={photoURL}
-                  alt=""
-                  className="object-cover w-12 h-12 rounded-full dark:bg-gray-500"
-                />
-              </div>
-              <div>
-                <h4 className="font-bold">{customerName}</h4>
-                <span className="text-xs dark:text-gray-400">
-                  {serviceName}
-                </span>
-              </div>
+const MyReviewRow = ({ rev, handleDelete }) => {
+  const { _id, serviceName, customerName, message, photoURL } = rev;
+  console.log(rev);
+  return (
+    <div>
+      <div className="container flex mt-6 flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 dark:bg-blue-900 dark:text-gray-100">
+        <div className="flex justify-between p-4">
+          <div className="flex space-x-4">
+            <div>
+              <img
+                src={photoURL}
+                alt=""
+                className="object-cover w-12 h-12 rounded-full dark:bg-gray-500"
+              />
+            </div>
+            <div>
+              <h4 className="font-bold">{customerName}</h4>
+              <span className="text-xs dark:text-gray-400">{serviceName}</span>
             </div>
           </div>
-          <div className="p-4 space-y-2 text-sm dark:text-gray-400">
-            <p>{message}</p>
+          <div className="flex flex-col ">
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn btn-square btn-outline"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <button className="btn mt-2 btn-ghost bg-none rounded-lg">Edit</button>
           </div>
         </div>
+        <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+          <p>{message}</p>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default MyReviewRow;
--- src/Pages/Review/MyReview/MyReview.js
+++ src/Pages/Review/MyReview/MyReview.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 import { AuthContext } from '../../../Contexts/AuthPovider/AuthProvider';
 import MyReviewRow from './MyRevieRow/MyReviewRow';
 
@@ -12,11 +13,35 @@ const MyReview = (props) => {
         .then((data) => setReview(data));
     }, [user?.email]);
 
+    const handleDelete = id => {
+        const proced = window.confirm("Are You are sure?");
+        if (proced) {
+            fetch(`http://localhost:5000/allreview/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        Swal.fire("Review deleted successfully");
+                        const remainning = review.filter(rew => rew._id !== id);
+                        setReview(remainning)
+                    }
+                })
+        }
+    }
     return (
-      <div className="grid grid-rogrid gap-2 py-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3ws-2">
-        {review.map((rev) => (
-          <MyReviewRow key={rev._id} rev={rev}></MyReviewRow>
-        ))}
+      <div>
+        <p className="text-2xl text-center">Total Review: {review.length}</p>
+        <div className="grid grid-rogrid gap-2 py-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3ws-2">
+          {review.map((rev) => (
+            <MyReviewRow
+              key={rev._id}
+                  rev={rev}
+                  handleDelete={handleDelete}
+            ></MyReviewRow>
+          ))}
+        </div>
       </div>
     );
 }
